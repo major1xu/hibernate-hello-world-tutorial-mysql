1088b21fd2291b5f12314621decce0b91969abe6
major1xu <minjie.xu@gmail.com>
2021-11-11 14:14:10 -0600

    interceptor

--- src/main/java/net/codejava/hibernate/ManageEmployee.java
+++ src/main/java/net/codejava/hibernate/ManageEmployee.java
@@ -10,9 +10,6 @@ import org.hibernate.HibernateException;
 import org.hibernate.Session; 
 import org.hibernate.Transaction;
 import org.hibernate.SessionFactory;
-import org.hibernate.Criteria;
-import org.hibernate.criterion.Restrictions;
-import org.hibernate.criterion.Projections;
 import org.hibernate.cfg.Configuration;
 
 public class ManageEmployee {
@@ -25,28 +22,31 @@ public class ManageEmployee {
          System.err.println("Failed to create sessionFactory object." + ex);
          throw new ExceptionInInitializerError(ex); 
       }
-      
+
       ManageEmployee ME = new ManageEmployee();
 
       /* Add few employee records in database */
-      Integer empID1 = ME.addEmployee("Zara", "Ali", 2000);
+      Integer empID1 = ME.addEmployee("Zara", "Ali", 1000);
       Integer empID2 = ME.addEmployee("Daisy", "Das", 5000);
-      Integer empID3 = ME.addEmployee("John", "Paul", 5000);
-      Integer empID4 = ME.addEmployee("Mohd", "Yasee", 3000);
+      Integer empID3 = ME.addEmployee("John", "Paul", 10000);
 
       /* List down all the employees */
       ME.listEmployees();
 
-      /* Print Total employee's count */
-      ME.countEmployee();
+      /* Update employee's records */
+      ME.updateEmployee(empID1, 5000);
 
-      /* Print Total salary */
-      ME.totalSalary();
+      /* Delete an employee from the database */
+      ME.deleteEmployee(empID2);
+
+      /* List down new list of the employees */
+      ME.listEmployees();
    }
    
    /* Method to CREATE an employee in the database */
    public Integer addEmployee(String fname, String lname, int salary){
-      Session session = factory.openSession();
+      Session session = factory.withOptions()
+              .interceptor(new MyInterceptor()).openSession();
       Transaction tx = null;
       Integer employeeID = null;
       
@@ -63,19 +63,16 @@ public class ManageEmployee {
       }
       return employeeID;
    }
-
-   /* Method to  READ all the employees having salary more than 2000 */
-   public void listEmployees( ) {
-      Session session = factory.openSession();
+   
+   /* Method to  READ all the employees */
+   public void listEmployees( ){
+      Session session = factory.withOptions()
+              .interceptor(new MyInterceptor()).openSession();
       Transaction tx = null;
       
       try {
          tx = session.beginTransaction();
-         Criteria cr = session.createCriteria(Employee.class);
-         // Add restriction.
-         cr.add(Restrictions.gt("salary", 2000));
-         List employees = cr.list();
-
+         List employees = session.createQuery("FROM Employee").list(); 
          for (Iterator iterator = employees.iterator(); iterator.hasNext();){
             Employee employee = (Employee) iterator.next(); 
             System.out.print("First Name: " + employee.getFirstName()); 
@@ -91,20 +88,17 @@ public class ManageEmployee {
       }
    }
    
-   /* Method to print total number of records */
-   public void countEmployee(){
-      Session session = factory.openSession();
+   /* Method to UPDATE salary for an employee */
+   public void updateEmployee(Integer EmployeeID, int salary ){
+      Session session = factory.withOptions()
+              .interceptor(new MyInterceptor()).openSession();
       Transaction tx = null;
       
       try {
          tx = session.beginTransaction();
-         Criteria cr = session.createCriteria(Employee.class);
-
-         // To get total row count.
-         cr.setProjection(Projections.rowCount());
-         List rowCount = cr.list();
-
-         System.out.println("Total Coint: " + rowCount.get(0) );
+         Employee employee = (Employee)session.get(Employee.class, EmployeeID); 
+         employee.setSalary( salary );
+		 session.update(employee); 
          tx.commit();
       } catch (HibernateException e) {
          if (tx!=null) tx.rollback();
@@ -113,21 +107,17 @@ public class ManageEmployee {
          session.close(); 
       }
    }
-  
-   /* Method to print sum of salaries */
-   public void totalSalary(){
-      Session session = factory.openSession();
+   
+   /* Method to DELETE an employee from the records */
+   public void deleteEmployee(Integer EmployeeID){
+      Session session = factory.withOptions()
+              .interceptor(new MyInterceptor()).openSession();
       Transaction tx = null;
       
       try {
          tx = session.beginTransaction();
-         Criteria cr = session.createCriteria(Employee.class);
-
-         // To get total salary.
-         cr.setProjection(Projections.sum("salary"));
-         List totalSalary = cr.list();
-
-         System.out.println("Total Salary: " + totalSalary.get(0) );
+         Employee employee = (Employee)session.get(Employee.class, EmployeeID); 
+         session.delete(employee); 
          tx.commit();
       } catch (HibernateException e) {
          if (tx!=null) tx.rollback();
